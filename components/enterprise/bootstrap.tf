0377b8039be02e64fc62752d68d6ba47d0d91406
Enda Kelly <enda.kelly@hmcts.net>
2022-08-30 10:25:39 +0100

    remove unnecessary vars

--- components/enterprise/bootstrap.tf
+++ components/enterprise/bootstrap.tf
@@ -2,10 +2,8 @@ module "bootstrap" {
   for_each = toset(var.cft_non_production_subscriptions)
 
   source = "../../modules/bootstrap"
-  name   = each.value
 
   storage_account_name = join("", ["c", each.value, "sa"])
   resource_group_name  = join("-", ["azure-control", var.environment, "rg"])
-  environment          = var.environment
   tags                 = module.tags.common_tags
 }
