7a936f2b18436e7bd40d945118a13e7fc7f41aab
Enda Kelly <enda.kelly@hmcts.net>
2022-08-30 12:53:38 +0100

    fix typo

--- modules/bootstrap/locals.tf
+++ modules/bootstrap/locals.tf
@@ -2,11 +2,11 @@ locals {
   groups = {
     owners = {
       name        = join(" ", ["DTS Owners", join("", ["(", join(":", ["sub", var.subscription_name]), ")"])]),
-      description = "Grants owner permissions to the ${var.subscription} subscription"
+      description = "Grants owner permissions to the ${var.subscription_name} subscription"
     }
     readers = {
       name        = join(" ", ["DTS Readers", join("", ["(", join(":", ["sub", var.subscription_name]), ")"])]),
-      description = "Grants reader permissions to the ${var.subscription} subscription"
+      description = "Grants reader permissions to the ${var.subscription_name} subscription"
     }
   }
 }
