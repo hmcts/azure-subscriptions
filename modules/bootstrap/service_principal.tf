968c894eebf3d515270c4824244066045d6ca25e
Enda Kelly <enda.kelly@hmcts.net>
2022-08-31 10:19:34 +0100

    fix application

--- modules/bootstrap/service_principal.tf
+++ modules/bootstrap/service_principal.tf
@@ -4,31 +4,21 @@ resource "random_password" "token" {
   override_special = "$@"
 }
 
+resource "random_uuid" "app_uuid" {}
+
 resource "azuread_application" "app" {
-  name     = local.app_name
-  homepage = "https://dev.azure.com/hmcts/Operations"
-  oauth2_permissions {
-    admin_consent_description = join(" ", [
-      "Allow the application to access",
-      local.app_name,
-      "on behalf of the signed-in user."
-    ])
-    admin_consent_display_name = join(" ", [
-      "Access",
-      local.app_name
-    ])
-    is_enabled = true
-    type       = "User"
-    user_consent_description = join(" ", [
-      "Allow the application to access",
-      local.app_name,
-      "on your behalf."
-    ])
-    user_consent_display_name = join(" ", [
-      "Access",
-      local.app_name
-    ])
-    value = "user_impersonation"
+  display_name = local.app_name
+  api {
+    oauth2_permissions_scope {
+      admin_consent_description  = join(" ", ["Allow the application to access", local.app_name, "on behalf of the signed-in user."])
+      admin_consent_display_name = join(" ", ["Access", local.app_name])
+      id                         = random_uuid.app_uuid.result
+      enabled                    = true
+      type                       = "User"
+      user_consent_description   = join(" ", ["Allow the application to access", local.app_name, "on your behalf."])
+      user_consent_display_name  = join(" ", ["Access", local.app_name])
+      value                      = "user_impersonation"
+    }
   }
   required_resource_access {
     resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
@@ -36,6 +26,9 @@ resource "azuread_application" "app" {
       id   = "06da0dbc-49e2-44d2-8312-53f166ab848a" # Directory.Read.All Delegated
       type = "Scope"
     }
+  }
+  web {
+    homepage_url = "https://dev.azure.com/hmcts/Operations"
   }
 }
 
