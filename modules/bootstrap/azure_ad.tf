e36f1d270356820179373b61efc85acd5fee3b68
Enda Kelly <enda.kelly@hmcts.net>
2022-08-31 08:37:43 +0100

    update resource names

--- modules/bootstrap/azure_ad.tf
+++ modules/bootstrap/azure_ad.tf
@@ -6,14 +6,14 @@ resource "azuread_group" "group" {
   security_enabled        = true
 }
 
-resource "azurerm_role_assignment" "role" {
+resource "azurerm_role_assignment" "local_groups" {
   for_each             = local.groups
   scope                = var.scope
   role_definition_name = each.key
   principal_id         = azuread_group.group[each.key].id
 }
 
-resource "azurerm_role_assignment" "role" {
+resource "azurerm_role_assignment" "local_role_assignments" {
   for_each             = local.role_assignments
   scope                = each.value.scope
   role_definition_name = each.key
