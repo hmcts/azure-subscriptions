d60f63573c702ddf48c8758bdb089e490cfd127e
Enda Kelly <enda.kelly@hmcts.net>
2022-08-31 09:39:00 +0100

    add kv access policy

--- modules/bootstrap/keyvault.tf
+++ modules/bootstrap/keyvault.tf
@@ -7,3 +7,67 @@ resource "azurerm_key_vault" "kv" {
   sku_name                 = var.sku_name
   tags                     = var.tags
 }
+
+resource "azurerm_key_vault_access_policy" "permissions" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azuread_group.group["Contributor"].id
+  certificate_permissions = [
+    "Backup",
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "ManageContacts",
+    "ManageIssuers",
+    "Recover",
+    "Restore",
+    "SetIssuers",
+    "Update"
+  ]
+  key_permissions = [
+    "Backup",
+    "Create",
+    "Decrypt",
+    "Delete",
+    "Encrypt",
+    "Get",
+    "Import",
+    "List",
+    "Recover",
+    "Restore",
+    "Sign",
+    "UnwrapKey",
+    "Update",
+    "Verify",
+    "WrapKey"
+  ]
+  secret_permissions = [
+    "Get",
+    "Set",
+    "List",
+    "Delete",
+    "Restore",
+    "Recover",
+    "Backup"
+  ]
+  storage_permissions = [
+    "Backup",
+    "Delete",
+    "DeleteSAS",
+    "Get",
+    "GetSAS",
+    "List",
+    "ListSAS",
+    "Recover",
+    "RegenerateKey",
+    "Restore",
+    "Set",
+    "SetSAS",
+    "Update"
+  ]
+}
