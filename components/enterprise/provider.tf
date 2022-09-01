6ea47060bd4fb5eb64b6d7ed9156491588fd7a77
Enda Kelly <enda.kelly@hmcts.net>
2022-09-01 09:51:16 +0100

    revert to vars in pipeline

--- components/enterprise/provider.tf
+++ components/enterprise/provider.tf
@@ -16,19 +16,6 @@ provider "azurerm" {
   features {}
 }
 
-provider "azuredevops" {
-  org_service_url       = "https://dev.azure.com/hmcts"
-  personal_access_token = data.azurerm_key_vault_secret.ado_token.value
-}
+provider "azuredevops" {}
 
 data "azurerm_client_config" "core" {}
-
-data "azurerm_key_vault" "infra_vault" {
-  name                = "infra-vault-${var.env}"
-  resource_group_name = "cnp-core-infra"
-}
-
-data "azurerm_key_vault_secret" "ado_token" {
-  name         = "azure-devops-token"
-  key_vault_id = data.azurerm_key_vault.infra_vault.id
-}
