57eb38fc0efccef877ddc1dd418c679f1c2ae76a
Enda Kelly <enda.kelly@hmcts.net>
2022-09-01 09:32:41 +0100

    add provider auth for devops

--- components/enterprise/provider.tf
+++ components/enterprise/provider.tf
@@ -16,6 +16,19 @@ provider "azurerm" {
   features {}
 }
 
-provider "azuredevops" {}
+provider "azuredevops" {
+  org_service_url = "https://dev.azure.com/hmcts"
+  personal_access_token = data.azurerm_key_vault_secret.ado_token.value
+}
 
 data "azurerm_client_config" "core" {}
+
+data "azurerm_key_vault" "infra_vault" {
+  name                = "infra-vault-${var.env}"
+  resource_group_name = "cnp-core-infra"
+}
+
+data "azurerm_key_vault_secret" "ado_token" {
+  name         = "azure-devops-token"
+  key_vault_id = data.azurerm_key_vault.infra_vault.id
+}
