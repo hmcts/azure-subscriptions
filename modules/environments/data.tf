ecb9a4a62fce340f4f7a89088900163da672ea24
Enda Kelly <enda.kelly@hmcts.net>
2022-09-06 08:53:33 +0100

    remvoe data resource

--- modules/environments/data.tf
+++ modules/environments/data.tf
@@ -1,3 +1,7 @@
 data "azuread_group" "acr" {
   name = "DTS ACR Access Administrators"
 }
+
+data "azuread_group" "dns_contributor" {
+  display_name = "DTS Public DNS Contributor (env:${lower(var.environment)})"
+}
