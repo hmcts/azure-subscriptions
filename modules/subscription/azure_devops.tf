f0e859dfc4baee753026c680d00966f2b96cbb64
Enda Kelly <enda.kelly@hmcts.net>
2022-09-06 08:50:18 +0100

    fix typo

--- modules/subscription/azure_devops.tf
+++ modules/subscription/azure_devops.tf
@@ -3,7 +3,7 @@ resource "azuredevops_serviceendpoint_azurerm" "endpoint" {
   service_endpoint_name = azurerm_subscription.this.subscription_name
   credentials {
     serviceprincipalid  = azuread_service_principal.sp.application_id
-    serviceprincipalkey = azuread_application_password.boostrap_token.value
+    serviceprincipalkey = azuread_application_password.token.value
   }
   azurerm_spn_tenantid      = data.azurerm_client_config.current.tenant_id
   azurerm_subscription_id   = azurerm_subscription.this.subscription_id
