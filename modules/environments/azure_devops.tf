39f58cc07513e4836a5028f972fe48d1dfa5d8e3
Enda Kelly <enda.kelly@hmcts.net>
2022-09-07 15:56:59 +0100

    change index of ptl sub

--- modules/environments/azure_devops.tf
+++ modules/environments/azure_devops.tf
@@ -20,8 +20,8 @@ resource "azuredevops_serviceendpoint_azurerm" "ptl_endpoint" {
     serviceprincipalkey = azuread_application_password.token.value
   }
   azurerm_spn_tenantid      = data.azurerm_client_config.current.tenant_id
-  azurerm_subscription_id   = data.azurerm_subscriptions.sds_ptl[0].subscriptions[0].subscription_id
-  azurerm_subscription_name = data.azurerm_subscriptions.sds_ptl[0].subscriptions[0].display_name
+  azurerm_subscription_id   = data.azurerm_subscriptions.sds_ptl[1].subscriptions[1].subscription_id
+  azurerm_subscription_name = data.azurerm_subscriptions.sds_ptl[1].subscriptions[1].display_name
 }
 
 resource "azuredevops_serviceendpoint_azurerm" "ptlsbox_endpoint" {
