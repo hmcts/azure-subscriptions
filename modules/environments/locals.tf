ed155bd4242a5a518885d915c5b7100f99c09669
Enda Kelly <enda.kelly@hmcts.net>
2022-09-08 08:18:43 +0100

    add missing bracket

--- modules/environments/locals.tf
+++ modules/environments/locals.tf
@@ -2,12 +2,12 @@ locals {
   app_name = "DTS Operations (env:${var.env}"
   groups = {
     "DTS Operations" = {
-      name        = "DTS Operations (env:${var.env}"
+      name        = "DTS Operations (env:${var.env})"
       description = "${var.env} Operations elevated access"
       members     = [azuread_service_principal.sp.object_id]
     }
     "DNS Zone Contributor" = {
-      name        = "DTS Public DNS Contributor (env:${var.env}"
+      name        = "DTS Public DNS Contributor (env:${var.env})"
       description = "Grants dns zone contributor permissions to the ${var.env} environment"
       members     = [azuread_service_principal.sp.object_id]
     }
