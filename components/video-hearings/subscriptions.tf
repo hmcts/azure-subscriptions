95db72769f57e5de1a8a3d4a072a39118baa3f6a
Enda Kelly <enda.kelly@hmcts.net>
2022-09-09 15:05:15 +0100

    update video hearings component

--- components/video-hearings/subscriptions.tf
+++ components/video-hearings/subscriptions.tf
@@ -9,10 +9,20 @@ locals {
 module "subscription" {
   for_each = local.subscriptions
 
-  source = "../../modules/subscription"
-  name   = each.key
-  value  = each.value
-
+  source                  = "../../modules/subscription"
+  name                    = each.key
+  value                   = each.value
+  common_tags             = module.tags[each.key].common_tags
+  dts_operations_group_id = module.environment[try(each.value.environment, (lower(replace([each.key][0], "/.*-([A-Za-z]+).*/", "$1"))))].dts_operations_group_id
+  environment             = try(each.value.environment, (lower(replace([each.key][0], "/.*-([A-Za-z]+).*/", "$1"))))
   billing_account_name    = var.billing_account_name
   enrollment_account_name = "TODO"
+}
+
+module "tags" {
+  for_each    = local.subscriptions
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=DTSPO-9743/azure-enterprise"
+  environment = try(each.value.environment, (lower(replace([each.key][0], "/.*-([A-Za-z]+).*/", "$1"))))
+  product     = var.product
+  builtFrom   = var.builtFrom
 }
