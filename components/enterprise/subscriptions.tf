50ba96053b7446e81797ebe7b97b38436f33af33
Enda Kelly <enda.kelly@hmcts.net>
2022-09-12 16:01:28 +0100

    update product for subscriptions

--- components/enterprise/subscriptions.tf
+++ components/enterprise/subscriptions.tf
@@ -32,6 +32,6 @@ module "tags" {
   for_each    = local.subscriptions
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=DTSPO-9743/azure-enterprise"
   environment = try(each.value.environment, (lower(replace([each.key][0], "/.*-([A-Za-z]+).*/", "$1"))))
-  product     = var.product
+  product     = lower(replace([each.key][0], "/([A-Za-z])_.*/", "$1"))
   builtFrom   = var.builtFrom
 }
