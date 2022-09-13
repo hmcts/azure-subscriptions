163c45d55a0d52b949fe3773156a467b0304677c
Enda Kelly <enda.kelly@hmcts.net>
2022-09-13 14:31:08 +0100

    update prevent_duplicate_names

--- modules/environments/groups.tf
+++ modules/environments/groups.tf
@@ -2,7 +2,7 @@ resource "azuread_group" "groups" {
   for_each                = local.groups
   display_name            = each.value.name
   description             = each.value.description
-  prevent_duplicate_names = false
+  prevent_duplicate_names = true
   security_enabled        = true
 
   members = each.value.members
