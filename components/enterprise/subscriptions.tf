7f0135f5f87e65813a5513104d5ddccc77e377ee
Enda Kelly <enda.kelly@hmcts.net>
2022-09-13 09:38:28 +0100

    add condition to tag product

--- components/enterprise/subscriptions.tf
+++ components/enterprise/subscriptions.tf
@@ -32,6 +32,6 @@ module "tags" {
   for_each    = local.subscriptions
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=DTSPO-9743/azure-enterprise"
   environment = try(each.value.environment, (lower(replace([each.key][0], "/.*-([A-Za-z]+).*/", "$1"))))
-  product     = try(each.value.product, (lower(replace([each.value.group][0], "/([A-Za-z])_.*/", "$1"))))
+  product     = try(each.value.product, (tostring(lower(replace([each.value.group][0], "/([A-Za-z])_.*/", "$1"))) == "cft" ? "cft-platform" : tostring(lower(replace([each.value.group][0], "/([A-Za-z])_.*/", "$1"))) == "sds" ? "sds-platform" : tostring(lower(replace([each.value.group][0], "/([A-Za-z])_.*/", "$1")))))
   builtFrom   = var.builtFrom
 }
