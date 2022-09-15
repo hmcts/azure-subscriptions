7297809c52e24a0a2e7c960352c9b8604bebef81
Enda Kelly <enda.kelly@hmcts.net>
2022-09-15 15:56:16 +0100

    remove commented code

--- modules/environments/azure_devops.tf
+++ modules/environments/azure_devops.tf
@@ -1,17 +1,3 @@
-# resource "azuredevops_project" "PlatformOperations" {
-#   name               = "PlatformOperations"
-#   visibility         = "private"
-#   work_item_template = "Agile"
-#   description        = "The Platform Operations team provides the creation, support, maintenance and enhancement of our Platform services and associated pipelines, with a focus on automation and self-service.  This enables project development and enduring product teams to deliver quality digital products rapidly, supporting the business as well as supporting and enhancing live services to citizens and internal staff."
-#   features = {
-#     "artifacts" = "disabled"
-#     "boards"    = "enabled"
-#     "pipelines" = "enabled"
-#     "repositories"     = "disabled"
-#     "testplans" = "disabled"
-#   }
-# }
-
 resource "azuredevops_serviceendpoint_azurerm" "endpoint" {
   count                 = var.pipeline_environment == "prod" ? 1 : 0
   project_id            = var.project_id
