8ba189d9add2681d6f56d55f6537d4e00bfa55df
Enda Kelly <enda.kelly@hmcts.net>
2022-09-15 15:32:36 +0100

    use new asp resource

--- modules/environments/service_plan.tf
+++ modules/environments/service_plan.tf
@@ -1,9 +1,10 @@
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   location            = var.location
   name                = "${var.product}-${var.env}-asp"
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "FunctionApp"
   reserved            = false
+  os_type             = "Windows"
   sku {
     size = var.asp_sku_size
     tier = var.asp_sku_tier
