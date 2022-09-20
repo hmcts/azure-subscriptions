4db4a3acf450cffe8d96ddc213f4ca97c94eadf6
endakelly <endakelly@users.noreply.github.com>
2022-09-20 15:37:32 +0000

    Apply automatic changes

--- modules/environments/azure_devops.tf
+++ modules/environments/azure_devops.tf
@@ -12,6 +12,6 @@ resource "azuredevops_serviceendpoint_azurerm" "endpoint" {
   # this creates issues when multiple subscriptions contain the same string e.g. `DTS-SHAREDSERIVCESPTL` and `DTS-SHAREDSERVICESPTL-SBOX`
   # see https://github.com/hashicorp/terraform-provider-azurerm/issues/18462
   # for expression below will extract the subscriptions and match the correct subscription id and display_name based on the variable display_name_prefix
-  azurerm_subscription_id   = [for elem in data.azurerm_subscriptions.service_endpoint_subscription[0].subscriptions[*]: elem.subscription_id if elem.display_name==var.display_name_prefix][0]
-  azurerm_subscription_name = [for elem in data.azurerm_subscriptions.service_endpoint_subscription[0].subscriptions[*]: elem.display_name if elem.display_name==var.display_name_prefix][0]
+  azurerm_subscription_id   = [for elem in data.azurerm_subscriptions.service_endpoint_subscription[0].subscriptions[*] : elem.subscription_id if elem.display_name == var.display_name_prefix][0]
+  azurerm_subscription_name = [for elem in data.azurerm_subscriptions.service_endpoint_subscription[0].subscriptions[*] : elem.display_name if elem.display_name == var.display_name_prefix][0]
 }
