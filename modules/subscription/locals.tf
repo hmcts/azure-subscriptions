3630d7c130929f7a03042ace70be40afed2608b6
Enda Kelly <enda.kelly@hmcts.net>
2022-09-21 10:43:50 +0100

    add pipeline service connection kv permissions

--- modules/subscription/locals.tf
+++ modules/subscription/locals.tf
@@ -14,7 +14,7 @@ locals {
     "Contributor" = {
       name        = "DTS Contributors (sub:${azurerm_subscription.this.subscription_name})"
       description = "Grants contributor permissions to the ${azurerm_subscription.this.subscription_name} subscription"
-      members     = [data.azuread_group.dts_operations.object_id, azuread_service_principal.sp.object_id]
+      members     = [data.azuread_group.dts_operations.object_id, azuread_service_principal.sp.object_id, data.azurerm_client_config.current.object_id]
     }
     "Key Vault Administrator" = {
       name        = "DTS Key Vault Administrators (sub:${azurerm_subscription.this.subscription_name})"
