1dd6d88fad5372cd14fc6b5a14309c30654ca6f7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-09-29 11:51:31 +0100

    update locals

--- modules/subscription/locals.tf
+++ modules/subscription/locals.tf
@@ -2,42 +2,42 @@ locals {
   app_name = "DTS Bootstrap (sub:${lower(azurerm_subscription.this.subscription_name)})"
   groups = {
     "Azure Kubernetes Service Cluster Admin Role" = {
-      name        = "DTS AKS Administrators (sub:${lower(azurerm_subscription.this.subscription_name)}))"
+      name        = "DTS AKS Administrators (sub:${lower(azurerm_subscription.this.subscription_name)})"
       description = "Grants aks cluster admin permissions to the ${azurerm_subscription.this.subscription_name} subscription"
       members     = [data.azuread_group.aks_global_admin.id]
     }
     "Azure Kubernetes Service Cluster User Role" = {
-      name        = "DTS AKS Users (sub:${lower(azurerm_subscription.this.subscription_name)}))"
+      name        = "DTS AKS Users (sub:${lower(azurerm_subscription.this.subscription_name)})"
       description = "Grants aks cluster user permissions to the ${azurerm_subscription.this.subscription_name} subscription"
       members     = []
     }
     "Contributor" = {
-      name        = "DTS Contributors (sub:${lower(azurerm_subscription.this.subscription_name)}))"
+      name        = "DTS Contributors (sub:${lower(azurerm_subscription.this.subscription_name)})"
       description = "Grants contributor permissions to the ${azurerm_subscription.this.subscription_name} subscription"
       members     = [data.azuread_group.dts_operations.object_id, azuread_service_principal.sp.object_id, data.azurerm_client_config.current.object_id]
     }
     "Key Vault Administrator" = {
-      name        = "DTS Key Vault Administrators (sub:${lower(azurerm_subscription.this.subscription_name)}))"
+      name        = "DTS Key Vault Administrators (sub:${lower(azurerm_subscription.this.subscription_name)})"
       description = "Grants key vault administrator permissions to the ${azurerm_subscription.this.subscription_name} subscription"
       members     = [data.azuread_group.dts_operations.object_id, azuread_service_principal.sp.object_id]
     }
     "Reader" = {
-      name        = "DTS Readers (sub:${lower(azurerm_subscription.this.subscription_name)}))"
+      name        = "DTS Readers (sub:${lower(azurerm_subscription.this.subscription_name)})"
       description = "Grants reader permissions to the ${azurerm_subscription.this.subscription_name} subscription"
       members     = [data.azuread_group.ops_mgmt.object_id]
     }
     "Security Reader" = {
-      name        = "DTS Security Readers (sub:${lower(azurerm_subscription.this.subscription_name)}))"
+      name        = "DTS Security Readers (sub:${lower(azurerm_subscription.this.subscription_name)})"
       description = "Grants security reader permissions to the ${azurerm_subscription.this.subscription_name} subscription"
       members     = []
     }
     "Storage Blob Data Reader" = {
-      name        = "DTS Blob Readers (sub:${lower(azurerm_subscription.this.subscription_name)}))"
+      name        = "DTS Blob Readers (sub:${lower(azurerm_subscription.this.subscription_name)})"
       description = "Grants storage blob data contributor permissions to the ${azurerm_subscription.this.subscription_name} subscription"
       members     = []
     }
     "Owner" = {
-      name        = "DTS Owners (sub:${lower(azurerm_subscription.this.subscription_name)}))"
+      name        = "DTS Owners (sub:${lower(azurerm_subscription.this.subscription_name)})"
       description = "Grants owner permissions to the ${azurerm_subscription.this.subscription_name} subscription"
       members     = [azuread_service_principal.sp.object_id]
     }
