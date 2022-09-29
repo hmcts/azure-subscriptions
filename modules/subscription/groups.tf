58870a35c85b5f03717db4a8aeb0033a5c200899
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-09-29 16:58:29 +0100

    add group members resource

--- modules/subscription/groups.tf
+++ modules/subscription/groups.tf
@@ -4,6 +4,10 @@ resource "azuread_group" "groups" {
   description             = each.value.description
   prevent_duplicate_names = true
   security_enabled        = true
+}
 
-  members = each.value.members
+resource "azuread_group_member" "members" {
+  for_each         = local.groups
+  group_object_id  = azuread_group.groups[each.key].object_id
+  member_object_id = each.value.members
 }
