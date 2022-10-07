6cab91228ea9f5eb974e1cb096982282275c6c60
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-07 15:06:24 +0100

    fix errors

--- components/acme/acme.tf
+++ components/acme/acme.tf
@@ -1,16 +1,21 @@
 module "acme" {
   for_each = { for k, v in local.subscriptions : k => v if try(v.deploy_acme, false) }
 
-  source = "git::https://github.com/hmcts/terraform-module-acme-function.git?ref=master"
+  source = "git::https://github.com/hmcts/terraform-module-acme-function.git?ref=DTSPO-9746/acme-kv"
 
   location                       = var.location
   env                            = try(each.value.environment, lower(replace([each.key][0], local.regex_last_section_hyphen, "$1")))
   common_tags                    = module.tags[each.key].common_tags
-  dns_contributor_group_id       = module.environment[try(each.value.environment, lower(replace([each.key][0], local.regex_last_section_hyphen, "$1")))].dns_contributor_group_id
+  dns_contributor_group_id       = "6c2d130e-ba66-408a-bdf9-f426a9730923"
   product                        = var.product
-  resource_group_name            = module.environment[try(each.value.environment, lower(replace([each.key][0], local.regex_last_section_hyphen, "$1")))].resource_group_name
-  resource_group_id              = module.environment[try(each.value.environment, lower(replace([each.key][0], local.regex_last_section_hyphen, "$1")))].resource_group_id
-  asp_id                         = module.environment[try(each.value.environment, lower(replace([each.key][0], local.regex_last_section_hyphen, "$1")))].asp_id
-  subscription_id                = module.subscription[each.key].subscription_id.subscription_id
-  acme_storage_account_repl_type = try(each.value.acme_storage_account_repl_type, "ZRS")
+  subscription_id                = [for elem in data.azurerm_subscriptions.subscription[0].subscriptions[*] : elem.subscription_id if elem.display_name == each.key]
+  acme_storage_account_repl_type = "ZRS"
+}
+
+module "tags" {
+  for_each    = local.subscriptions
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = try(each.value.environment, lower(replace([each.key][0], local.regex_last_section_hyphen, "$1")))
+  product     = try(each.value.product, replace(regex("cft_|sds_", [each.value.group][0]), local.regex_first_section_underscore, "$1-platform"), replace(regex("security", [each.value.group][0]), local.regex_string, "soc"), replace([each.value.group][0], local.regex_first_section_underscore, "$1"))
+  builtFrom   = var.builtFrom
 }
---
+++ components/acme/data.tf
@@ -0,0 +1,6 @@
+data "azurerm_subscriptions" "subscription" {
+  for_each = { for k, v in local.subscriptions : k => v if try(v.deploy_acme, false) }
+  # Environment service connections are associated to the SDS subscriptions
+  # because they need to be associated to at least one even if you are using a different one
+  display_name_prefix = each.key
+}
