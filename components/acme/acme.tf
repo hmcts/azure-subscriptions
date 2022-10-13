56cc775856b95c272aff63b1e15482232b160bf0
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-13 11:14:13 +0100

    removed unused vars

--- components/acme/acme.tf
+++ components/acme/acme.tf
@@ -1,13 +1,14 @@
 module "acme" {
   for_each = { for k, v in local.subscriptions : k => v if k == data.azurerm_subscription.current.display_name && try(v.deploy_acme, false) }
-
   source = "git::https://github.com/hmcts/terraform-module-acme-function.git?ref=DTSPO-9746/acme-kv"
+
+  providers = { 
+    azurerm = azurerm[each.key]
+  }
 
   location                       = var.location
   env                            = try(each.value.environment, lower(replace([each.key][0], local.regex_last_section_hyphen, "$1")))
   common_tags                    = module.tags[each.key].common_tags
-  platform_operations_object_id  = "e7ea2042-4ced-45dd-8ae3-e051c6551789"
-  dns_contributor_group_id       = "6c2d130e-ba66-408a-bdf9-f426a9730923"
   product                        = var.product
   subscription_id                = data.azurerm_client_config.current.subscription_id
   acme_storage_account_repl_type = "ZRS"
