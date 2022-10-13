72b49f56b3a79b3c161682c5b1c24dd3c0070c0e
endakelly <endakelly@users.noreply.github.com>
2022-10-13 10:05:56 +0000

    Apply automatic changes

--- components/acme/acme-providers.tf
+++ components/acme/acme-providers.tf
@@ -1,8 +1,8 @@
-data "azurerm_subscriptions" "dcd_cft_sandbox"{
-    display_name_prefix = "DCD-CFT-Sandbox"
+data "azurerm_subscriptions" "dcd_cft_sandbox" {
+  display_name_prefix = "DCD-CFT-Sandbox"
 }
 
 provider "azurerm" {
-  alias = "DCD-CFT-Sandbox"
+  alias           = "DCD-CFT-Sandbox"
   subscription_id = data.azurerm_subscriptions.dcd_cft_sandbox.id
 }
