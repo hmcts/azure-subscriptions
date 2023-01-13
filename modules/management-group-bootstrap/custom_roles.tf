365c3099723a9b990b8340a72f9e8064c55055ed
matt.slater <matt.slater@justice.gov.uk>
2023-01-13 16:39:02 +0000

    DTSPO-11394 - output role definitions to be able to pass to subscription module

--- modules/management-group-bootstrap/custom_roles.tf
+++ modules/management-group-bootstrap/custom_roles.tf
@@ -1,8 +1,21 @@
-resource "azurerm_role_definition" "app_gateway_backend_health_reader" {
-  name        = "Application Gateway Backend Health Reader"
-  description = "View backend health on the Application Gateway"
-  scope       = "/providers/Microsoft.Management/managementGroups/HMCTS"
+resource "azurerm_role_definition" "custom_role_definitions" {
+  for_each    = local.custom_role_assignments
+
+  name        = each.key
+  description = each.value.description
+  scope       = each.value.scope
+
   permissions {
-    actions = ["Microsoft.Network/applicationGateways/backendhealth/action"]
+    actions = each.value.actions
+  }
+}
+
+locals {
+  custom_role_assignments = {
+    "Application Gateway Backend Health Reader" = {
+      description = "View backend health on the Application Gateway"
+      scope       = "/providers/Microsoft.Management/managementGroups/HMCTS"
+      actions     = ["Microsoft.Network/applicationGateways/backendhealth/action"]
+    },
   }
 }
---
+++ modules/management-group-bootstrap/outputs.tf
@@ -0,0 +1,3 @@
+output "custom_roles" {
+  value = azurerm_role_definition.custom_role_definitions[*]
+}
