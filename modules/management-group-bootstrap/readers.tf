bc7ff3ee299aaacf8ad0d3ea0cb4d4b79efd674e
matt.slater <matt.slater@justice.gov.uk>
2023-01-16 19:37:18 +0000

    DTSPO-11394 - custom roles for management groups

--- modules/management-group-bootstrap/readers.tf
+++ modules/management-group-bootstrap/readers.tf
@@ -16,13 +16,13 @@ resource "azurerm_role_assignment" "readers" {
 }
 
 
-resource "azurerm_role_assignment" "app_gateway_backend_health_reader" {
-  for_each = var.groups
-
-  principal_id       = azuread_group.readers[each.value.id].object_id
-  scope              = "/providers/Microsoft.Management/managementGroups/${each.value.id}"
-  role_definition_id = "159469f4-6e46-4b09-aa73-8e2f243aa784"
-}
+#resource "azurerm_role_assignment" "app_gateway_backend_health_reader" {
+#  for_each = var.groups
+#
+#  principal_id       = azuread_group.readers[each.value.id].object_id
+#  scope              = "/providers/Microsoft.Management/managementGroups/${each.value.id}"
+#  role_definition_id = "159469f4-6e46-4b09-aa73-8e2f243aa784"
+#}
 
 locals {
   custom_role_assignments = [
@@ -32,7 +32,7 @@ locals {
 
 
 resource "azurerm_role_assignment" "custom_role_assignments_readers" {
-  for_each = [for group in var.groups : { for k, role in local.custom_role_assignments : group => k }]
+  for_each = [for group_key, group_value in var.groups : { for role in local.custom_role_assignments : group => role }]
 
   principal_id       = azuread_group.readers[each.key].object_id
   scope              = "/providers/Microsoft.Management/managementGroups/${each.key}"
