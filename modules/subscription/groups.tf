db509476a1afa3bc1d4505f16e227dbfe9bb7ce8
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-06-21 12:13:23 +0100

    run in prod only

--- modules/subscription/groups.tf
+++ modules/subscription/groups.tf
@@ -13,6 +13,7 @@ resource "azuread_group_member" "members" {
 }
 
 resource "azuread_group_member" "dts_operation_members" {
+  count            = var.env == "prod" ? 1 : 0
   group_object_id  = "DTS Operations (env: ${var.environment})"
   member_object_id = azuread_service_principal.sp.object_id
 }
