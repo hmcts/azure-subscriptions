5743d7cc4d047725ec688c5add3953a7a4f2f87e
nicholas chang <nicholasc001@hotmail.com>
2023-09-15 14:38:00 +0100

    edit provider.tf

--- components/enterprise/provider.tf
+++ components/enterprise/provider.tf
@@ -14,11 +14,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  client_id       = "25c5cb78-93f1-415e-a4d7-b269d0b24ff9"
-  client_secret   = "zpw8Q~Kn~rAKIvCfe1rS6WpN-CWHWLZtYNIMha.E"
-  tenant_id       = "531ff96d-0ae9-462a-8d2d-bec7c0b42082"
-  subscription_id = "ae75b9fb-7d34-4112-82ff-64bd3855ce27"
 }
 
 provider "azuredevops" {}
