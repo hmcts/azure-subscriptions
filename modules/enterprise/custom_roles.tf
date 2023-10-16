dff9f1a987e91f726e3a29e4785ec6528399beb2
Alex Bance <Alex.Bance@justice.gov.uk>
2023-10-16 10:37:20 +0100

    fix: add depends on to ensure subscriptions are added to a mgmt group before trying to reference custom roles from the mgmt group

--- modules/enterprise/custom_roles.tf
+++ modules/enterprise/custom_roles.tf
@@ -8,4 +8,13 @@ resource "azurerm_role_definition" "custom_role_definitions" {
   permissions {
     actions = each.value.actions
   }
+
+  depends_on = [
+    azurerm_management_group.level_1,
+    azurerm_management_group.level_2,
+    azurerm_management_group.level_3,
+    azurerm_management_group.level_4,
+    azurerm_management_group.level_5,
+    azurerm_management_group.level_6
+  ]
 }
