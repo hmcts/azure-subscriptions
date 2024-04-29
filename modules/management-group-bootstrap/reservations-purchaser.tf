b643352dec667b978374aa52bb7c32f7e61865db
Thomas Thornton <>
2024-04-29 13:25:27 +0100

    add reservations purchaser

---
+++ modules/management-group-bootstrap/reservations-purchaser.tf
@@ -0,0 +1,16 @@
+resource "azuread_group" "reservation_purchaser" {
+  for_each = var.groups
+
+
+  display_name            = "DTS Reservations Purchaser (mg:${each.value.id})"
+  prevent_duplicate_names = true
+  security_enabled        = true
+}
+
+resource "azurerm_role_assignment" "reservation_purchaser" {
+  for_each = var.groups
+
+  principal_id         = azuread_group.reservation_purchaser[each.value.id].object_id
+  scope                = "/providers/Microsoft.Management/managementGroups/${each.value.id}"
+  role_definition_name = "Reservation Purchaser"
+}
