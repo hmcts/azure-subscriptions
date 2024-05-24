1ff34c195d0b783065f595adbff1dd1d198d9298
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-05-24 14:56:36 +0100

    DTSPO-17071 - remove use of PAT token

--- modules/subscription/provider.tf
+++ modules/subscription/provider.tf
@@ -2,8 +2,7 @@ terraform {
   required_version = "1.6.1"
   required_providers {
     azuredevops = {
-      source  = "microsoft/azuredevops"
-      version = "0.10.0"
+      source = "microsoft/azuredevops"
     }
   }
 }
