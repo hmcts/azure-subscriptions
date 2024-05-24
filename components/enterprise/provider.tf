1779055b7791d5baebff4093765e754ccf17b9ab
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-05-24 17:26:07 +0100

    Testing with SP

--- components/enterprise/provider.tf
+++ components/enterprise/provider.tf
@@ -16,6 +16,10 @@ provider "azurerm" {
   features {}
 }
 
-provider "azuredevops" {}
+provider "azuredevops" {
+  org_service_url = "https://dev.azure.com/hmcts"
+  client_id       = "10936009-a112-4733-bb2a-94ee240b79ff" # azure-devops-sp
+  tenant_id       = "531ff96d-0ae9-462a-8d2d-bec7c0b42082"
+}
 
 data "azurerm_client_config" "core" {}
